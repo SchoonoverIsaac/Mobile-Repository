9e59cdc2aa23d12b63b581ffbd1941150d68f4d1
SchoonoverIsaac <schoonoverisaac@gmail.com>
2026-01-21 13:30:58 -0800

    colectables script

--- My project/Assets/scripts/GasAndBrakeV1.cs
+++ My project/Assets/scripts/GasAndBrakeV1.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GasAndBrakeV1 : MonoBehaviour
+public class GasAndBrake : MonoBehaviour
 {
+    public float Gas = 5f;
+    public float Brake = -5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class GasAndBrakeV1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // when gas button is pressed move forword//
+       
         
     }
 }
