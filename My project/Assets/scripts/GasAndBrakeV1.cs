daf52dbfb3ade76782d8a1c0a72cc0c29de2cbf4
SchoonoverIsaac <schoonoverisaac@gmail.com>
2026-01-28 13:57:07 -0800

    break and gas

--- My project/Assets/scripts/GasAndBrakeV1.cs
+++ My project/Assets/scripts/GasAndBrakeV1.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GasAndBrake : MonoBehaviour
 {
+    Rigidbody2D rb;
     public float Gas = 5f;
+    public float Brake = 5f;
+    [SerializeField] private InputActionReference moveActionToUse;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       // when gas button is pressed move forword//
-       
+        // when gas button is pressed move forword//
+        float movex = Input.GetAxis("Vertical");
+        Vector2 vector21 = moveActionToUse.action.ReadValue<Vector2>();
+        Vector2 vector2 = vector21;
+        Vector2 moveDirection = vector2;
+        if (Input.GetButtonDown("gas"))
+        {
+
+            rb.AddForce(new Vector2(0, 100 * Gas));
+
+        }
+        
+
         
     }
     public void GasPedal()
     {
-
+        Debug.Log("gas called");
+        rb.AddForce(new Vector2(0, 100 * Gas));
     }
 }
