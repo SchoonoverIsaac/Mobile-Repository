bb3f6274ebdf2e33a689e8bdc0001d8dcfded3f1
Siege-Xcalibur <27.israel.olivera@ksd.org>
2026-01-30 13:20:34 -0800

    Finished the code tto the Rival AI Waypoints

--- My project/Assets/scripts/Rival AI.cs
+++ My project/Assets/scripts/Rival AI.cs
@@ -6,6 +6,10 @@ public class RivalAI : MonoBehaviour
 {
     [Range(0f,2f)]
     [SerializeField] private float WayPointSize = 1f;
+
+    [Header("Path Settings")]
+    // Sets the path to be looped so agent will go form the last waypoint to the first or vice versa
+    [SerializeField] private bool canLoop = true;
     private void OnDrawGizmos()
     {
         foreach (Transform t in transform)
@@ -18,9 +22,17 @@ public class RivalAI : MonoBehaviour
         {
           Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
         }
-        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+
+        // If the path is set to loop then draw a line between the last and first waypoint
+        if (canLoop)
+        {
+            Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+        }
+
+       
     }
 
+    // will get the correct next waypoint based on the direction currently travelling
     public Transform GetNextWaypoint(Transform currentWaypoint)
     {
         if (currentWaypoint == null) 
@@ -34,7 +46,14 @@ public class RivalAI : MonoBehaviour
         }
        else
         {
-            return transform.GetChild(0);
+            if (canLoop)
+            {
+                return transform.GetChild(0);
+            }
+            else 
+            { 
+              return transform.GetChild(currentWaypoint.GetSiblingIndex());
+            }
         }
     }   
 }
