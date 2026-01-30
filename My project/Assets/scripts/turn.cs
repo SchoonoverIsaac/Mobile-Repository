001e146bd3e6aac69057dd907a517eb48b4e1efe
SchoonoverIsaac <schoonoverisaac@gmail.com>
2026-01-30 13:02:28 -0800

    finish

--- My project/Assets/scripts/turn.cs
+++ My project/Assets/scripts/turn.cs
@@ -2,77 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class turn: MonoBehaviour
+
+public class Turn : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public float jumpSpeed = 5f;
-    bool grounded = false;
-    //where
-    AudioSource audioSource;
-    //what sound do we want to play
-    public AudioClip jumpSound;
+    public float left = 5f;
+    public float right = 5f;
     [SerializeField] private InputActionReference moveActionToUse;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        audioSource = Camera.main.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //when we press left or right, move the char left/right
-        float moveX = Input.GetAxis("Horizontal");
+     
+        float movex = Input.GetAxis("Horizontal");
         Vector2 vector21 = moveActionToUse.action.ReadValue<Vector2>();
         Vector2 vector2 = vector21;
         Vector2 moveDirection = vector2;
-        //maintain the integrity of our Y velocity
-        Vector3 velocity = rb.velocity;
-        velocity.x = moveDirection.x * moveSpeed;
-        rb.velocity = velocity;
-        // if you press the space key AND you're on the ground, jump the char
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            if (audioSource != null && jumpSound != null)
-            {
-                //play the jump sound
-                audioSource.PlayOneShot(jumpSound);
-            }
-            rb.AddForce(new Vector2(0, 100 * jumpSpeed));
-        }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+
+
+
+
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    public void rightturn ()
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = false;
-        }
+        Debug.Log("right called");
+      
     }
-    public void Jump()
+    public void leftturn()
     {
-        Debug.Log("jump called");
-        if (grounded)
-        {
-            if (audioSource != null && jumpSound != null)
-            {
-                //play the jump sound
-                audioSource.PlayOneShot(jumpSound);
-            }
-            rb.AddForce(new Vector2(0, 100 * jumpSpeed));
-        }
-    }
-    public void UpgradeSpeed()
-    {
-        moveSpeed += 0.1f;
+        Debug.Log("left called");
+      
     }
 }
